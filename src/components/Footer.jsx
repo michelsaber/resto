98d917e7c80f0457d21d6c8c31c2d58c803efc5c
michelsaber <michel-saber@hotmail.com>
2023-11-02 20:33:47 +0100

    setting up the project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Button} from "react-bootstrap"
+
+function Footer() {
+  return (
+    <div><p>Footer</p>
+         <Button>click here</Button>
+    </div>
+  )
+}
+
+export default Footer
